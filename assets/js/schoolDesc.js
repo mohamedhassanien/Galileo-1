7018f3d0d073f0f083319227e37cd306738bcad8
Aya <ayafikry33@gmail.com>
2021-03-23 14:23:23 +0200

    third

--- assets/js/schoolDesc.js
+++ assets/js/schoolDesc.js
@@ -1,3 +1,37 @@
+// CITY PHOTOS SLIDER
+// Create and mount the thumbnails slider.
+var citySecondarySlider = new Splide( '#city-secondary-slider', {
+	rewind      : true,
+	fixedWidth  : 100,
+	fixedHeight : 64,
+	isNavigation: true,
+    arrows      : true,
+	gap         : 10,
+	focus       : 'center',
+	pagination  : false,
+	cover       : true,
+	breakpoints : {
+		'600': {
+			fixedWidth  : 66,
+			fixedHeight : 40,
+		}
+	}
+} ).mount();
+
+// Create the main slider.
+var cityPrimarySlider = new Splide( '#city-primary-slider', {
+	type       : 'fade',
+	heightRatio: 0.5,
+	pagination : false,
+	arrows     : false,
+	cover      : true,
+} );
+
+// Set the thumbnails slider as a sync target and then call mount.
+cityPrimarySlider.sync( citySecondarySlider ).mount();
+
+
+// SCHOOL PHOTOS SLIDER
 // Create and mount the thumbnails slider.
 var secondarySlider = new Splide( '#secondary-slider', {
 	rewind      : true,
@@ -37,7 +71,7 @@ var reviewSecondarySlider = new Splide( '#review-secondary-slider', {
 	fixedHeight : 64,
 	isNavigation: true,
 	arrows      : false,
-	focus       : 'center',
+	// focus       : 'center',
 	pagination  : false,
 	cover       : true,
 	breakpoints : {
@@ -59,10 +93,3 @@ var reviewPrimarySlider = new Splide( '#review-primary-slider', {
 
 // Set the thumbnails slider as a sync target and then call mount.
 reviewPrimarySlider.sync( reviewSecondarySlider ).mount();
-document.querySelectorAll('#review-secondary-slider-list .splide__slide')
-.forEach( slide => { 
-	if(slide.classList = 'is-active'){
-		this.style.order = 2
-		
-	}
-})
