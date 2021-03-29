fb995a177be7de4d5d26f5d3b0380b653a360ffa
Aya <ayafikry33@gmail.com>
2021-03-29 13:54:31 +0200

    program Filtration

---
+++ assets/js/programs.js
@@ -0,0 +1,15 @@
+//MULTI SELECT FIELD
+new SlimSelect({
+    select: '#field'
+})
+document.getElementsByClassName('field-multiple')[1].querySelector('span.ss-disabled').innerText = 'Choose one field or more'
+//MULTI SELECT SCHOOL
+new SlimSelect({
+    select: '#school'
+})
+document.getElementsByClassName('school-multiple')[1].querySelector('span.ss-disabled').innerText = 'Choose one school or more'
+//MULTI SELECT CITY
+new SlimSelect({
+    select: '#City'
+})
+document.getElementsByClassName('city-multiple')[1].querySelector('span.ss-disabled').innerText = 'Choose one city or more'
